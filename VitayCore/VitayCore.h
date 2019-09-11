8af02055bf59d5c838182a2b9911e9a6cfdb9777
Juan Pablo <juanpablomr.014@gmail.com>
2019-09-11 18:04:03 -0500

    Initial Commit

---
+++ VitayCore/VitayCore.h
@@ -0,0 +1,19 @@
+//
+//  VitayCore.h
+//  VitayCore
+//
+//  Created by Juan Pablo on 9/11/19.
+//  Copyright © 2019 Vitay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VitayCore.
+FOUNDATION_EXPORT double VitayCoreVersionNumber;
+
+//! Project version string for VitayCore.
+FOUNDATION_EXPORT const unsigned char VitayCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VitayCore/PublicHeader.h>
+
+
